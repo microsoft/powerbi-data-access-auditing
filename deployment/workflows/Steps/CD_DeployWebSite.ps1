abcf25a457c53320cee437be12a9faefa0169773
John Rampono <john.rampono@microsoft.com>
2021-07-14 14:20:33 +0800

    Pipeline changes

--- deployment/workflows/Steps/CD_DeployWebSite.ps1
+++ deployment/workflows/Steps/CD_DeployWebSite.ps1
@@ -1,7 +1,7 @@
 
 if ($env:AdsOpts_CD_Services_WebSite_Enable -eq "True")
-{   
-    if ($env:AdsOpts_CD_Services_AppPlans_WebApp_ResourceGroup -eq $null)
+{            
+    if ($null -eq $env:AdsOpts_CD_Services_AppPlans_WebApp_ResourceGroup)
     {
         $rg = $env:AdsOpts_CD_ResourceGroup_Name
     }
@@ -13,7 +13,7 @@ if ($env:AdsOpts_CD_Services_WebSite_Enable -eq "True")
     
     $sn = $env:AdsOpts_CD_Services_AppPlans_WebApp_Name
     
-    Write-Host "Deploying Wesite to $sn in resource group $rg" 
+    Write-Host "Deploying Website to $sn in resource group $rg" 
     az deployment group create -g $rg --template-file ./../arm/WebApp.json  --parameters location=$env:AdsOpts_CD_ResourceGroup_Location sites_AdsGoFastWebApp_name=$env:AdsOpts_CD_Services_WebSite_Name appservice-name=$sn}
 else 
 {
