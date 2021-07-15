e92bf47e33594259e45ecd7925810dcd8dac571f
John Rampono <john.rampono@microsoft.com>
2021-07-15 16:12:08 +0800

    Fixed resource group hash issue

--- deployment/workflows/Steps/CD_SetResourceGroupHash.ps1
+++ deployment/workflows/Steps/CD_SetResourceGroupHash.ps1
@@ -1,7 +1,12 @@
 
+function Get-UniqueString ([string]$id, $length=13)
+{
+    $hashArray = (new-object System.Security.Cryptography.SHA512Managed).ComputeHash($id.ToCharArray())
+        -join ($hashArray[1..$length] | ForEach-Object { [char]($_ % 26 + [byte][char]'a') })
+}
 
 Write-Host "Creating RG Hash"
-$ResourceGroupHash =uniqueString($env:AdsOpts_CD_ResourceGroup_Name)
+$ResourceGroupHash = Get-UniqueString ($id=$env:AdsOpts_CD_ResourceGroup_Name)
 Write-Host $ResourceGroupHash
 
 PersistEnvVariable -Name "AdsOpts_CD_ResourceGroup_Hash" -Value $ResourceGroupHash
