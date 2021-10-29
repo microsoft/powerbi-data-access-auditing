3e341afbe08f2e3e46e12e4563bc2ea065a71b39
Emlyn Rice <84910463+emlyn-arkahna@users.noreply.github.com>
2021-10-29 13:46:58 +0800

    Remove duplicate test files
    Add error handling
    Add error emails

--- src/PowerBiAuditProcessor.Tests/OutputTests.cs
+++ src/PowerBiAuditProcessor.Tests/OutputTests.cs
@@ -1,8 +1,14 @@
 using System.IO;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using PowerBiAuditProcessor.Tests.Fakers;
+using SendGrid.Helpers.Mail;
 using VerifyTests;
 using VerifyXunit;
 using Xunit;
@@ -36,7 +42,7 @@ public class OutputTests
         var container = new FakeContainerClient();
 
 
-        await PowerBiAuditLogProcessor.Run(filePath.Replace(ExamplesFolder, ""), blob, container, new Mock<ILogger>().Object);
+        await PowerBiAuditLogProcessor.Run(filePath.Replace(ExamplesFolder, ""), blob, container, new Mock<IAsyncCollector<SendGridMessage>>().Object, new Mock<ILogger>().Object);
 
         foreach (var (resultFileName, file) in container.GetFiles())
         {
@@ -46,6 +52,51 @@ public class OutputTests
             await Verifier.Verify(file, settings);
         }
 
+    }
+
+    [Theory]
+    [MemberData(nameof(ExampleFiles))]
+    public async Task InputHasAdditionalProperty_WhenTheProcessIsRun_AndErrorEmailIsSent(string filePath)
+    {
+        //Arrange
+        var jsonString = await File.ReadAllTextAsync(filePath);
+        var jObject = JsonConvert.DeserializeObject<JObject>(jsonString);
+        jObject!["AProperty"] = "An empty string";
+
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(jObject.ToString()));
+        var blob = new FakeBlockBlobClient(stream);
+        var container = new FakeContainerClient();
+        var collectorMock = new Mock<IAsyncCollector<SendGridMessage>>();
+
+        // Act
+        await PowerBiAuditLogProcessor.Run(filePath.Replace(ExamplesFolder, ""), blob, container, collectorMock.Object, new Mock<ILogger>().Object);
+
+
+        //Assert
+        collectorMock.Verify(x => x.AddAsync(It.IsAny<SendGridMessage>(), It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+
+    [Theory]
+    [MemberData(nameof(ExampleFiles))]
+    public async Task InputIsMissingAProperty_WhenTheProcessIsRun_AndErrorEmailIsSent(string filePath)
+    {
+        //Arrange
+        var jsonString = await File.ReadAllTextAsync(filePath);
+        var jObject = JsonConvert.DeserializeObject<JObject>(jsonString);
+        jObject!["Response"]!["results"]!.Parent!.Remove();
+
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(jObject.ToString()));
+        var blob = new FakeBlockBlobClient(stream);
+        var container = new FakeContainerClient();
+        var collectorMock = new Mock<IAsyncCollector<SendGridMessage>>();
+
+        // Act
+        await PowerBiAuditLogProcessor.Run(filePath.Replace(ExamplesFolder, ""), blob, container, collectorMock.Object, new Mock<ILogger>().Object);
+
+
+        //Assert
+        collectorMock.Verify(x => x.AddAsync(It.IsAny<SendGridMessage>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 
 }
--- src/PowerBiAuditProcessor/PowerBiAuditLogProcessor.cs
+++ src/PowerBiAuditProcessor/PowerBiAuditLogProcessor.cs
@@ -14,6 +14,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using PowerBiAuditProcessor.Models;
+using SendGrid.Helpers.Mail;
 using DataRow = PowerBiAuditProcessor.Models.DataRow;
 using DataSet = PowerBiAuditProcessor.Models.DataSet;
 
@@ -26,44 +27,93 @@ public static class PowerBiAuditLogProcessor
         [QueueTrigger("audit-log-queue", Connection = "AzureWebJobsStorage")] string name,
         [Blob("audit-pre-processed-log-storage/{queueTrigger}", FileAccess.Read, Connection = "AzureWebJobsStorage")] BlockBlobClient auditBlobClient,
         [Blob("audit-processed-log-storage", Connection = "AzureWebJobsStorage")] BlobContainerClient processedLogClient,
+        [SendGrid] IAsyncCollector<SendGridMessage> messageCollector,
         ILogger log
         )
     {
-        var model = await ReadJson(name, auditBlobClient, log);
-
-        foreach (var result in model.Response.Results)
+        AuditLog model;
+        try
         {
-            var headerLookup = result.Result.Data.Descriptor.Select.ToDictionary(x => x.Value);
-            foreach (var dataSet in result.Result.Data.Dsr.DataSets)
+            model = await ReadJson(name, auditBlobClient, log);
+        }
+        catch (Exception exception)
+        {
+            await FailWithError(messageCollector, $"Reading auditLog failed for: {name}", auditBlobClient, exception, log);
+            return;
+        }
+
+        try
+        {
+            foreach (var result in model.Response.Results)
             {
-                foreach (var (key, data) in dataSet.Ph.SelectMany(x => x))
+                var headerLookup = result.Result.Data.Descriptor.Select.ToDictionary(x => x.Value);
+                foreach (var dataSet in result.Result.Data.Dsr.DataSets)
                 {
-                    // If no data was returned don't log it
-                    if (!data.Any())
+                    foreach (var (key, data) in dataSet.Ph.SelectMany(x => x))
                     {
-                        log.LogWarning("Did not find any data in {fileName}:{key}", name, key);
-                        continue;
+                        await WriteCsv(name, auditBlobClient, processedLogClient, messageCollector, log, data, key, result, dataSet, headerLookup);
                     }
-
-                    var filename = $"{name} ({result.JobId}-{dataSet.Name}-{key}).csv";
-                    var blob = processedLogClient.GetBlockBlobClient(filename);
-
-                    await using var stream = await blob.OpenWriteAsync(true);
-                    await using var writer = new StreamWriter(stream);
-                    await using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) { LeaveOpen = false, HasHeaderRecord = false });
-
-                    var headers = data.Single(x => x.ColumnHeaders is not null).ColumnHeaders;
-
-                    WriteHeaders(headers, headerLookup, csvWriter);
-                    WriteRows(data, headers, dataSet, csvWriter);
-
-                    await csvWriter.FlushAsync();
-                    log.LogInformation("Data written for {name} ({result.JobId}-{dataSet.Name}-{key}).csv", name, result.JobId, dataSet.Name, key);
                 }
             }
         }
+        catch (Exception exception)
+        {
+            await FailWithError(messageCollector, $"Outputting auditLog failed for: {name}", auditBlobClient, exception, log);
+            return;
+        }
 
         await auditBlobClient.DeleteAsync();
+    }
+
+    private static async Task WriteCsv(string name, BlockBlobClient auditBlobClient, BlobContainerClient processedLogClient,
+        IAsyncCollector<SendGridMessage> messageCollector, ILogger log, DataRow[] data, string key, ResultElement result, DataSet dataSet,
+        Dictionary<string, DescriptorSelect> headerLookup)
+    {
+        try
+        {
+            // If no data was returned don't log it
+            if (!data.Any())
+            {
+                log.LogWarning("Did not find any data in {fileName}:{key}", name, key);
+                return;
+            }
+
+            var filename = $"{name} ({result.JobId}-{dataSet.Name}-{key}).csv";
+            var blob = processedLogClient.GetBlockBlobClient(filename);
+
+            await using var stream = await blob.OpenWriteAsync(true);
+            await using var writer = new StreamWriter(stream);
+            await using var csvWriter = new CsvWriter(writer,
+                new CsvConfiguration(CultureInfo.InvariantCulture) { LeaveOpen = false, HasHeaderRecord = false });
+
+            var headers = data.Single(x => x.ColumnHeaders is not null).ColumnHeaders;
+
+            WriteHeaders(headers, headerLookup, csvWriter);
+            WriteRows(data, headers, dataSet, csvWriter);
+
+            await csvWriter.FlushAsync();
+            log.LogInformation("Data written for {name} ({result.JobId}-{dataSet.Name}-{key}).csv", name, result.JobId,
+                dataSet.Name, key);
+        }
+        catch (Exception exception)
+        {
+            await FailWithError(messageCollector, $"Writing CSC data failed for: {name} ({result.JobId}-{dataSet.Name}-{key})", auditBlobClient, exception, log);
+        }
+    }
+
+    private static async Task FailWithError(IAsyncCollector<SendGridMessage> messageCollector, string message, BlockBlobClient auditBlobClient, Exception exception, ILogger log)
+    {
+        var sendGridMessage = new SendGridMessage {
+            From = new EmailAddress(Environment.GetEnvironmentVariable("ErrorFromEmailAddress", EnvironmentVariableTarget.Process)),
+            Subject = message,
+            PlainTextContent = $"{message}\r\n in file {auditBlobClient.Uri}\r\n Please investigate."
+        };
+        sendGridMessage.AddTo(new EmailAddress(Environment.GetEnvironmentVariable("ErrorToEmailAddress", EnvironmentVariableTarget.Process)));
+
+
+        await messageCollector.AddAsync(sendGridMessage);
+        await messageCollector.FlushAsync();
+        log.LogError(exception, message);
     }
 
     private static async Task<AuditLog> ReadJson(string name, BlockBlobClient auditBlobClient, ILogger log)
