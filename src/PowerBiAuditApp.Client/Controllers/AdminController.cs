54584ae9d097146f20fc772825b22d69c4ad4cbb
Michael Biesheuvel <michael.biesheuvel@arkahna.io>
2021-11-11 16:22:59 +0800

    Add admin page with queue trigger

---
+++ src/PowerBiAuditApp.Client/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PowerBiAuditApp.Client.Services;
+
+namespace PowerBiAuditApp.Client.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IQueueTriggerService _queueTriggerService;
+
+        public AdminController(IQueueTriggerService queueTriggerService)
+        {
+            _queueTriggerService = queueTriggerService;
+        }
+
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> RefreshReports()
+        {
+            // Send message to trigger queue & redirect back to admin page
+            var queueClient = await _queueTriggerService.GetTriggerQueue();
+
+            var filename = $"{Guid.NewGuid()} {DateTime.UtcNow:yyyy-MM-dd hh-mm-ss}.json";
+
+            await _queueTriggerService.SendQueueMessage(queueClient, filename);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> CreateTriggerQueue()
+        {
+            await _queueTriggerService.GetTriggerQueue();
+
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ src/PowerBiAuditApp.Client/Services/QueueTriggerService.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Options;
+using PowerBiAuditApp.Client.Models;
+
+namespace PowerBiAuditApp.Client.Services;
+
+public class QueueTriggerService : IQueueTriggerService
+{
+    private readonly QueueServiceClient _queueServiceClient;
+    private readonly IOptions<StorageAccountSettings> _settings;
+
+    public QueueTriggerService(IOptions<StorageAccountSettings> settings, QueueServiceClient queueServiceClient)
+    {
+        _settings = settings;
+        _queueServiceClient = queueServiceClient;
+    }
+    
+    public async Task<QueueClient> GetTriggerQueue(CancellationToken cancellationToken = default)
+    {
+        var client = _queueServiceClient.GetQueueClient(_settings.Value.TriggerQueueName?.ToLower());
+        await client.CreateIfNotExistsAsync(cancellationToken: cancellationToken);
+        return client;
+    }
+
+    public async Task SendQueueMessage(QueueClient queueClient, string message, CancellationToken cancellationToken = default) =>
+        await queueClient.SendMessageAsync(message, cancellationToken: cancellationToken);
+}
+
