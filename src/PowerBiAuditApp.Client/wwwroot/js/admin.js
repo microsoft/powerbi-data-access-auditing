393da6dd77a9b71bf452e8508fadd54da4f92552
Michael Biesheuvel <michael.biesheuvel@arkahna.io>
2021-11-18 13:26:22 +0800

    Move admin JavaScript to file

--- src/PowerBiAuditApp.Client/wwwroot/js/admin.js
+++ src/PowerBiAuditApp.Client/wwwroot/js/admin.js
@@ -1,8 +1,27 @@
-﻿function SubmitForm(form) {
-    form.preventDefault();
+﻿var original = $('#reportDisplayForm').serialize();
 
-    var data = { "query": $('form').serialize() };
+$('#reportDisplayForm').on('change', function () {
+    if ($('#reportDisplayForm').serialize() != original) {
+        $('button[type="submit"]').attr("disabled", false);
+    } else {
+        $('button[type="submit"]').attr("disabled", true);
+    };
+});
+
+$(window).on('beforeunload', function () {
+    if ($('#reportDisplayForm').serialize() != original) {
+        return "Are you want to leave before saving your changes?";
+    }
+});
+
+$('#reportDisplayForm').submit(function (event) {
+    event.preventDefault();
+
+    var data = { "query": $('#reportDisplayForm').serialize() };
     $.post("/Admin/SaveReportDisplayDetails", data);
 
     console.log("Form submitted");
-}
+
+    $('button[type="submit"]').attr("disabled", true);
+    original = $('#reportDisplayForm').serialize();
+});
